7d8435d9611eaaf778c7c7e4a45dec0f20b46abe
arthurcekindo <arthur.immanuel@cekindo.com>
2022-01-23 22:39:41 +0700

    Write JSON File

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	logFile, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := logFile.Write([]byte("Menambahkan data\n")); err != nil {
+		log.Fatal(err)
+	}
+	if err := logFile.Close(); err != nil {
+		log.Fatal(err)
+	}
+	data := logFile
+	output := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(output)
+
+	file, _ := json.MarshalIndent(data, "", " ")
+
+	_ = ioutil.WriteFile("sample.json", file, 0644)
 }
